da2bb8abd7b19bac53cdebc85e2c1710fdac659f
Maksim Remin <reminmax@gmail.com>
2023-01-10 04:25:39 +0200

    Create DI module for ViewModel

---
+++ app/src/main/java/com/reminmax/apollographql/di/ViewModelModule.kt
@@ -0,0 +1,19 @@
+package com.reminmax.apollographql.di
+
+import com.reminmax.apollographql.data.repository.CharacterRepository
+import com.reminmax.apollographql.data.repository.ICharacterRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class ViewModelModule {
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindRepository(repo: CharacterRepository): ICharacterRepository
+
+}
