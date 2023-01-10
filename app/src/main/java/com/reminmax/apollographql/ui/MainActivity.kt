8179ce7e8682332c8c83fa745647342e43904102
Maksim Remin <reminmax@gmail.com>
2023-01-10 04:28:48 +0200

    MainActivity moved to UI package

--- app/src/main/java/com/reminmax/apollographql/ui/MainActivity.kt
+++ app/src/main/java/com/reminmax/apollographql/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.reminmax.apollographql
+package com.reminmax.apollographql.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.reminmax.apollographql.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
