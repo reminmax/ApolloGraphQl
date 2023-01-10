f2502d23b41252c5848b634ab4ec8a66ada003e0
Maksim Remin <reminmax@gmail.com>
2023-01-10 22:58:46 +0200

    Main activity implementation

--- app/src/main/java/com/reminmax/apollographql/ui/MainActivity.kt
+++ app/src/main/java/com/reminmax/apollographql/ui/MainActivity.kt
@@ -2,11 +2,36 @@ package com.reminmax.apollographql.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.reminmax.apollographql.R
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.reminmax.apollographql.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
