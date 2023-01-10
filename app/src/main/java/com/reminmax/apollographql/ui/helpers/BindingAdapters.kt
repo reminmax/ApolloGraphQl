c3ab882318a7164e9fab4ee302b2a38175c6da08
Maksim Remin <reminmax@gmail.com>
2023-01-11 00:23:57 +0200

    Change name of the binding adapter

--- app/src/main/java/com/reminmax/apollographql/ui/helpers/BindingAdapters.kt
+++ app/src/main/java/com/reminmax/apollographql/ui/helpers/BindingAdapters.kt
@@ -6,7 +6,7 @@ import com.apollographql.apollo3.api.ApolloResponse
 import com.reminmax.apollographql.CharactersListQuery
 import com.reminmax.apollographql.ui.state.ViewState
 
-@BindingAdapter("isVisible")
+@BindingAdapter("showWhenLoading")
 fun bindVisible(view: View, uiState: ViewState<ApolloResponse<CharactersListQuery.Data>>) {
     view.visibility = if (uiState is ViewState.Loading)
         View.VISIBLE
