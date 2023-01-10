906b692cd8d97d47b668971ef9f3c7ce05bc10bc
Maksim Remin <reminmax@gmail.com>
2023-01-10 04:19:24 +0200

    Add CharacterRepository implementation

--- app/src/main/java/com/reminmax/apollographql/data/repository/CharacterRepository.kt
+++ app/src/main/java/com/reminmax/apollographql/data/repository/CharacterRepository.kt
@@ -2,11 +2,15 @@ package com.reminmax.apollographql.data.repository
 
 import com.apollographql.apollo3.api.ApolloResponse
 import com.reminmax.apollographql.CharactersListQuery
+import com.reminmax.apollographql.data.network.RickAndMortyApi
+import javax.inject.Inject
 
-class CharacterRepository : ICharacterRepository {
+class CharacterRepository @Inject constructor(
+    private val webService: RickAndMortyApi
+) : ICharacterRepository {
 
     override suspend fun queryCharactersList(): ApolloResponse<CharactersListQuery.Data> {
-        TODO("Not yet implemented")
+        return webService.getApolloClient().query(CharactersListQuery()).execute()
     }
 
 }
