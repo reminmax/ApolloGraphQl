a44a70eb5d2189e54afc997b546673b110920f3d
Maksim Remin <reminmax@gmail.com>
2023-01-10 23:02:58 +0200

    Change repository implementation

--- app/src/main/java/com/reminmax/apollographql/data/repository/CharacterRepository.kt
+++ app/src/main/java/com/reminmax/apollographql/data/repository/CharacterRepository.kt
@@ -1,16 +1,15 @@
 package com.reminmax.apollographql.data.repository
 
+import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.ApolloResponse
 import com.reminmax.apollographql.CharactersListQuery
-import com.reminmax.apollographql.data.network.RickAndMortyApi
 import javax.inject.Inject
 
 class CharacterRepository @Inject constructor(
-    private val webService: RickAndMortyApi
+    private val apolloClient: ApolloClient
 ) : ICharacterRepository {
 
     override suspend fun queryCharactersList(): ApolloResponse<CharactersListQuery.Data> {
-        return webService.getApolloClient().query(CharactersListQuery()).execute()
+        return apolloClient.query(CharactersListQuery()).execute()
     }
-
 }
--- app/src/main/java/com/reminmax/apollographql/data/repository/ICharacterRepository.kt
+++ app/src/main/java/com/reminmax/apollographql/data/repository/ICharacterRepository.kt
@@ -2,9 +2,11 @@ package com.reminmax.apollographql.data.repository
 
 import com.apollographql.apollo3.api.ApolloResponse
 import com.reminmax.apollographql.CharactersListQuery
+import kotlinx.coroutines.flow.Flow
 
 interface ICharacterRepository {
 
+    //suspend fun queryCharactersList(): Flow<ApolloResponse<CharactersListQuery.Data>>
     suspend fun queryCharactersList(): ApolloResponse<CharactersListQuery.Data>
 
 }
