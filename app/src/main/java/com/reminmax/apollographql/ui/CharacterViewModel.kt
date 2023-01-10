a9d55bf48a85b77b7d7c8f8aa9a5edb067bf8e34
Maksim Remin <reminmax@gmail.com>
2023-01-10 23:10:17 +0200

    Change ViewModel implementation

--- app/src/main/java/com/reminmax/apollographql/ui/CharacterViewModel.kt
+++ app/src/main/java/com/reminmax/apollographql/ui/CharacterViewModel.kt
@@ -1,35 +1,42 @@
 package com.reminmax.apollographql.ui
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.exception.ApolloException
 import com.reminmax.apollographql.CharactersListQuery
+import com.reminmax.apollographql.R
+import com.reminmax.apollographql.Utils.WhileUiSubscribed
 import com.reminmax.apollographql.data.repository.CharacterRepository
+import com.reminmax.apollographql.di.IoDispatcher
+import com.reminmax.apollographql.ui.helpers.resource_provider.IResourceProvider
 import com.reminmax.apollographql.ui.state.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.*
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterViewModel @Inject constructor(
     private val repository: CharacterRepository,
+    private val resourceProvider : IResourceProvider,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<ViewState<ApolloResponse<CharactersListQuery.Data>>>(ViewState.Loading())
-    val uiState: StateFlow<ViewState<ApolloResponse<CharactersListQuery.Data>>> = _uiState
-
-    fun queryCharactersList() = viewModelScope.launch {
-        _uiState.emit(ViewState.Loading())
+    val uiState: StateFlow<ViewState<ApolloResponse<CharactersListQuery.Data>>> = flow {
         try {
-            val response = repository.queryCharactersList()
-            _uiState.emit(ViewState.Success(response))
-        } catch (e: ApolloException) {
-            Log.d("ApolloException", "Failure", e)
-            _uiState.emit(ViewState.Error("Error occurred while fetching data"))
+            emit(ViewState.Success(repository.queryCharactersList()))
+        } catch (ex: ApolloException) {
+            Timber.e(ex)
+            emit(ViewState.Error(resourceProvider.getString(R.string.errorOccurredWhileFetchingData)))
         }
-    }
+    }.flowOn(ioDispatcher)
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileUiSubscribed,
+            initialValue = ViewState.Loading()
+        )
+
+
 }
