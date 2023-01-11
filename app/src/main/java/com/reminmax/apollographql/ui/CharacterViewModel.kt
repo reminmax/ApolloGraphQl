f9a4c38926279cc27c9b4c47d45c2753c27db163
Maksim Remin <reminmax@gmail.com>
2023-01-11 04:16:19 +0200

    ViewModel implementation

--- app/src/main/java/com/reminmax/apollographql/ui/CharacterViewModel.kt
+++ app/src/main/java/com/reminmax/apollographql/ui/CharacterViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.exception.ApolloException
+import com.reminmax.apollographql.CharacterQuery
 import com.reminmax.apollographql.CharactersListQuery
 import com.reminmax.apollographql.R
 import com.reminmax.apollographql.Utils.WhileUiSubscribed
@@ -14,23 +15,30 @@ import com.reminmax.apollographql.ui.state.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterViewModel @Inject constructor(
     private val repository: CharacterRepository,
-    private val resourceProvider : IResourceProvider,
+    private val resourceProvider: IResourceProvider,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-    val uiState: StateFlow<ViewState<ApolloResponse<CharactersListQuery.Data>>> = flow {
+    private val _characterState =
+        MutableStateFlow<ViewState<ApolloResponse<CharacterQuery.Data>>>(ViewState.Loading())
+    val characterState = _characterState.asStateFlow()
+
+    val characterListState: StateFlow<ViewState<ApolloResponse<CharactersListQuery.Data>>> = flow {
         try {
             emit(ViewState.Success(repository.queryCharactersList()))
         } catch (ex: ApolloException) {
             Timber.e(ex)
-            emit(ViewState.Error(
-                message = resourceProvider.getString(R.string.errorOccurredWhileFetchingData))
+            emit(
+                ViewState.Error(
+                    message = resourceProvider.getString(R.string.errorOccurredWhileFetchingData)
+                )
             )
         }
     }.flowOn(ioDispatcher)
@@ -40,5 +48,14 @@ class CharacterViewModel @Inject constructor(
             initialValue = ViewState.Loading()
         )
 
-
+    fun loadCharacterState(id: String) {
+        viewModelScope.launch(ioDispatcher) {
+            try {
+                _characterState.emit(ViewState.Success(repository.queryCharacter(id)))
+            } catch (ex: ApolloException) {
+                Timber.e(ex)
+                _characterState.emit(ViewState.Error(message = ex.message))
+            }
+        }
+    }
 }
