cd2422169e6e6de3be92a73363dffe9d3d1a1e84
Maksim Remin <reminmax@gmail.com>
2023-01-11 04:17:36 +0200

    Add fragment to show character details

---
+++ app/src/main/java/com/reminmax/apollographql/ui/character_details/CharacterDetailsFragment.kt
@@ -0,0 +1,63 @@
+package com.reminmax.apollographql.ui.character_details
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.reminmax.apollographql.R
+import com.reminmax.apollographql.databinding.FragmentCharacterDetailsBinding
+import com.reminmax.apollographql.ui.CharacterViewModel
+import com.reminmax.apollographql.ui.base.BaseFragment
+import com.reminmax.apollographql.ui.state.ViewState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class CharacterDetailsFragment :
+    BaseFragment<FragmentCharacterDetailsBinding>(R.layout.fragment_character_details) {
+
+    private val viewModel: CharacterViewModel by activityViewModels()
+    private val args: CharacterDetailsFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        collectUiState()
+    }
+
+    private fun initView() {
+        viewModel.loadCharacterState(args.id)
+        binding.viewModel = viewModel
+    }
+
+    private fun collectUiState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.characterState
+                .flowWithLifecycle(
+                    lifecycle = viewLifecycleOwner.lifecycle,
+                    minActiveState = Lifecycle.State.STARTED
+                )
+                .collect { viewState ->
+                    when (viewState) {
+                        is ViewState.Success -> {
+                            if (viewState.value?.data?.character == null) {
+                                binding.characterDetailsFetchProgress.visibility = View.GONE
+                            } else {
+                                binding.query = viewState.value.data
+                                binding.characterDetailsFetchProgress.visibility = View.GONE
+                            }
+                        }
+                        is ViewState.Error -> {
+                            Toast.makeText(context, viewState.message, Toast.LENGTH_SHORT).show()
+                        }
+                        is ViewState.Loading -> Unit
+                    }
+                }
+        }
+    }
+}
--- app/src/main/java/com/reminmax/apollographql/ui/helpers/BindingAdapters.kt
+++ app/src/main/java/com/reminmax/apollographql/ui/helpers/BindingAdapters.kt
@@ -1,8 +1,11 @@
 package com.reminmax.apollographql.ui.helpers
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import coil.load
 import com.apollographql.apollo3.api.ApolloResponse
+import com.reminmax.apollographql.CharacterQuery
 import com.reminmax.apollographql.CharactersListQuery
 import com.reminmax.apollographql.ui.state.ViewState
 
@@ -13,9 +16,33 @@ fun bindShowWhenLoading(view: View, uiState: ViewState<ApolloResponse<Characters
     else View.GONE
 }
 
+@BindingAdapter("showWhenDetailsLoading")
+fun bindShowWhenDetailsLoading(view: View, uiState: ViewState<ApolloResponse<CharacterQuery.Data>>) {
+    view.visibility = if (uiState is ViewState.Loading)
+        View.VISIBLE
+    else View.GONE
+}
+
 @BindingAdapter("hideIfErrorOccurred")
-fun bindHideIfErrorOccurred(view: View, uiState: ViewState<ApolloResponse<CharactersListQuery.Data>>) {
+fun bindHideIfErrorOccurred(
+    view: View,
+    uiState: ViewState<ApolloResponse<CharactersListQuery.Data>>
+) {
     view.visibility = if (uiState is ViewState.Error)
         View.GONE
     else View.VISIBLE
+}
+
+@BindingAdapter("imageUrl")
+fun setImageUrl(imageView: ImageView, url: String?) {
+    imageView.load(url) {
+        crossfade(true)
+    }
+}
+
+@BindingAdapter("showWhenError")
+fun bindShowWhenError(view: View, uiState: ViewState<ApolloResponse<CharacterQuery.Data>>) {
+    view.visibility = if (uiState is ViewState.Error)
+        View.VISIBLE
+    else View.GONE
 }
