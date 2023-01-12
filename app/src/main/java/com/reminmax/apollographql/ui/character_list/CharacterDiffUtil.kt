699143c23fb3f160259104760fa7dbb4d977529d
Maksim Remin <reminmax@gmail.com>
2023-01-13 00:23:57 +0200

    DiffUtil refactoring

--- app/src/main/java/com/reminmax/apollographql/ui/character_list/CharacterDiffUtil.kt
+++ app/src/main/java/com/reminmax/apollographql/ui/character_list/CharacterDiffUtil.kt
@@ -3,19 +3,15 @@ package com.reminmax.apollographql.ui.character_list
 import androidx.recyclerview.widget.DiffUtil
 import com.reminmax.apollographql.CharactersListQuery
 
-class CharacterDiffUtil : DiffUtil.ItemCallback<CharactersListQuery.Result>() {
+object CharacterDiffUtil : DiffUtil.ItemCallback<CharactersListQuery.Result>() {
 
     override fun areItemsTheSame(
         oldItem: CharactersListQuery.Result,
         newItem: CharactersListQuery.Result
-    ): Boolean {
-        return oldItem.id == newItem.id
-    }
+    ) = oldItem.id == newItem.id
 
     override fun areContentsTheSame(
         oldItem: CharactersListQuery.Result,
         newItem: CharactersListQuery.Result
-    ): Boolean {
-        return oldItem == newItem
-    }
+    ) = oldItem == newItem
 }
